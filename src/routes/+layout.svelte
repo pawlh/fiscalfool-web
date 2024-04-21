48c2333ee2abdcc3616da33d5be6f14a9197cb8b
Paul Hathaway <paulchathaway@gmail.com>
2024-04-21 17:24:40 -0600

    init base layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "./globals.css"
+</script>
+
+<div class="app">
+  <slot />
+</div>
+
+<style>
+  .app {
+    height: 100dvh;
+    width: 100dvw;
+
+    color: var(--color-text);
+    background-color: var(--color-background-primary);
+  }
+</style>
