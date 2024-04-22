03565a195b5e47e0cac6be01eb3c5d1aef2fd761
Paul Hathaway <paulchathaway@gmail.com>
2024-04-22 13:41:43 -0600

    consolidate styles

--- src/routes/SidePanel.svelte
+++ src/routes/SidePanel.svelte
@@ -1,13 +1,29 @@
+<script>
+	import Icon from '$lib/Icon.svelte';
+	import { faTimes } from '@fortawesome/free-solid-svg-icons';
+</script>
+
 <div class="side-panel visible">
-	<header class="side-panel__header">
-		<h1>Here is a header</h1>
-	</header>
-	<div class="side-panel__container">
-		<p>here is some content</p>
+	<div class="side-panel__content">
+		<header class="side-panel__header">
+			<h1>Here is a header</h1>
+			<span class="side-panel__close">
+				<Icon icon={faTimes} style="font-size: var(--icon-size);" />
+			</span>
+		</header>
+		<div class="side-panel__container">
+			<p>here is some content</p>
+		</div>
 	</div>
 </div>
 
 <style>
+	.side-panel__close {
+		position: absolute;
+		top: var(--spacing-small);
+		right: var(--spacing-small);
+	}
+
 	.side-panel {
 		visibility: hidden;
 		transition: visibility 0s 0.6s;
@@ -18,29 +34,9 @@
 		}
 	}
 
-	.side-panel__header {
+	.side-panel__content {
 		position: fixed;
 		top: 0;
-		left: 0;
-
-		width: 80%;
-		height: 50px;
-
-		transition: transform 0.3s 0s;
-		transform: translateY(-50px);
-
-		/* centered partial border on bottom */
-		border-bottom: 1px solid var(--color-text);
-
-		.visible & {
-			transition: transform 0.3s 0.3s;
-			transform: translateY(0px);
-		}
-	}
-
-	.side-panel__container {
-		position: fixed;
-		top: 50px;
 		left: 0;
 
 		width: 80%;
@@ -49,14 +45,21 @@
 		transition: transform 0.3s 0s;
 		transform: translate3d(-100%, 0, 0);
 
+		background-color: var(--color-background-secondary);
+
 		.visible & {
 			transform: translate3d(0, 0, 0);
 			transition-delay: 0s;
 		}
 	}
 
-	.side-panel > * {
-		background-color: var(--color-background-secondary);
-		color: var(--color-text);
+	.side-panel__header {
+		border-bottom: 1px solid var(--color-text);
+
+		h1 {
+			font-size: 1.5rem;
+			margin: 0;
+			padding: 1rem;
+		}
 	}
 </style>
