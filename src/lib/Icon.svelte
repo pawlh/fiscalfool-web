0da7bfdddc0ee0477bafedbbfb1f65fad188a356
Paul Hathaway <paulchathaway@gmail.com>
2024-04-21 23:23:11 -0600

    create wrapper for FontAwesomeIcon

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { FontAwesomeIcon, type FontAwesomeIconProps } from '@fortawesome/svelte-fontawesome';
+
+	type Props = FontAwesomeIconProps & {
+		onclick?: (e: any) => void;
+	};
+
+	let { onclick, ...props }: Props = $props();
+</script>
+
+{#if onclick}
+	<button onclick={onclick as (e: any) => void} onkeydown={onclick}>
+		<FontAwesomeIcon {...props} />
+	</button>
+{:else}
+	<FontAwesomeIcon {...props} />
+{/if}
+
+<style>
+	button {
+		background: none;
+		border: none;
+		cursor: pointer;
+		color: var(--color-text);
+	}
+</style>
