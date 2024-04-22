763af2708f6729dc931062104c98a06f54e46876
Paul Hathaway <paulchathaway@gmail.com>
2024-04-21 23:29:06 -0600

    add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,19 @@
 <script>
-  import "./globals.css"
+	import NavBar from './NavBar.svelte';
+	import './globals.css';
 </script>
 
 <div class="app">
-  <slot />
+	<NavBar showBack={true} />
+	<slot />
 </div>
 
 <style>
-  .app {
-    height: 100%;
-    width: 100%;
+	.app {
+		height: 100%;
+		width: 100%;
 
-    border: 1px solid transparent;
-
-    color: var(--color-text);
-    background-color: var(--color-background-primary);
-  }
+		color: var(--color-text);
+		background-color: var(--color-background-primary);
+	}
 </style>
