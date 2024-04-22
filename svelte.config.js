069c8858c682c288d2994307cc79cef97dd63c91
Paul Hathaway <paulchathaway@gmail.com>
2024-04-21 22:28:46 -0600

    add legacy component support

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,13 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	compilerOptions: {
+		legacy: {
+			// https://svelte-5-preview.vercel.app/docs/breaking-changes#components-are-no-longer-classes
+			componentApi: true
+		}
 	}
 };
 
