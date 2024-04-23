3268979a1c12adf97f60f5ef8bd4149cfeac55a0
Paul Hathaway <paulchathaway@gmail.com>
2024-04-23 15:09:13 -0600

    add base grid layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import NavBar from './NavBar.svelte';
+	import SidePanel from './SidePanel.svelte';
 	import './globals.css';
 </script>
 
 <div class="app">
-	<NavBar showBack={true} />
-	<slot />
+	<div class="navbar"><NavBar showBack={true} /></div>
+	<div class="sidepanel"><SidePanel isOpen={true} /></div>
+	<div class="main"><slot /></div>
 </div>
 
 <style>
@@ -15,5 +17,34 @@
 
 		color: var(--color-text);
 		background-color: var(--color-background-primary);
+		border: 1px solid var(--color-text);
+	}
+
+	@media (min-width: 768px) {
+		.app {
+			display: grid;
+			grid-template-columns: auto 1fr;
+			grid-template-rows: auto 1fr;
+			gap: 0px 0px;
+			grid-template-areas:
+				'NavBar NavBar'
+				'SidePanel Main';
+		}
+	}
+
+	.navbar {
+		grid-area: NavBar;
+
+		background-color: var(--color-background-secondary);
+	}
+
+	.sidepanel {
+		grid-area: SidePanel;
+
+		background-color: var(--color-background-secondary);
+	}
+
+	.main {
+		grid-area: Main;
 	}
 </style>
